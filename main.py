4831989b003e2b0f920d89199f796331e2dc4ad2
ptofga <ptofga@hotmail.com>
2022-04-07 11:09:40 +0800

    implment render pass statistic

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ symbol_list = []
 peak_drawcall_no = []
 # read threshold file and get threshold
 with open("threshold.txt") as f:
-    threshold = float(f.readline())
+	threshold = float(f.readline())
 
 # read information file
 info_df = pandas.read_csv("Information.csv")
@@ -19,14 +19,14 @@ info_df = pandas.read_csv("Information.csv")
 
 # read GPU duration from each metric data exported from GPA and make new DataFrame
 for (index, row) in info_df.iterrows():
-    numberofGPU += 1
-    temp_df = pandas.read_csv(row.FileName)
-    if row.Brand == "Intel" or row.Brand == "intel":
-        comparison_df[row.Platform] = temp_df["GPU Duration"]
-    else:
-        comparison_df[row.Platform] = temp_df["GPU Elapsed Time"]
-    platform_list.append(row.Platform)
-    mean = comparison_df[row.Platform].values.mean()
+	numberofGPU += 1
+	temp_df = pandas.read_csv(row.FileName)
+	if row.Brand == "Intel" or row.Brand == "intel":
+		comparison_df[row.Platform] = temp_df["GPU Duration"]
+	else:
+		comparison_df[row.Platform] = temp_df["GPU Elapsed Time"]
+	platform_list.append(row.Platform)
+	mean = comparison_df[row.Platform].values.mean()
 
 plt.rcParams['figure.figsize'] = (16.0, 4.0)
 comparison_df.plot()
@@ -34,42 +34,81 @@ comparison_df.plot()
 
 # get all No. of Draw Call which performance is huge different
 def get_peaks(dataframe):
-    peak_list = []
-    for (index, value) in dataframe.iterrows():
+	peak_list = []
+	for (index, value) in dataframe.iterrows():
         # get max and min of value
         #print(index)
-        temp_list = []
-        for i in range(numberofGPU):
-            temp_list.append(float(value[i]))
-        if max(temp_list) / max(min(temp_list),1) >= threshold and max(temp_list) >= mean:
-            peak_list.append((index, max(temp_list)))
-            peak_drawcall_no.append(index)
-    return peak_list
+		temp_list = []
+		for i in range(numberofGPU):
+			temp_list.append(float(value[i]))
+		if max(temp_list) / max(min(temp_list),1) >= threshold and max(temp_list) >= mean:
+			peak_list.append((index, max(temp_list)))
+			peak_drawcall_no.append(index)
+	return peak_list
 
 # check if symbol file exist
 if os.path.exists("symbol.csv"):
-    symbol_df = pandas.read_csv("symbol.csv")
-    for (no, value) in symbol_df.iterrows():
-        str_list = value.Region.split('\\')
-        symbol_list.append(str_list[-1])
+	symbol_df = pandas.read_csv("symbol.csv")
+	for (no, value) in symbol_df.iterrows():
+		str_list = value.Region.split('\\')
+		symbol_list.append(str_list[-1])
 # write symbol to compare dateframe
-    comparison_df['Symbol'] = symbol_list
+	comparison_df['Symbol'] = symbol_list
+
+#  statistic cost by render pass
+#init render pass name	
+	renderpassname= ""
+	#init accumulate list 
+	accumulate_list = [0,0,0]
+	dict_list = []
+	dict = {}
+	for( index, row) in comparison_df.iterrows() : 
+		if renderpassname == "" :
+			renderpassname = row.Symbol
+			for i in range(numberofGPU):
+				accumulate_list[i]= int(row[platform_list[i]])
+		else :
+			if renderpassname == row.Symbol : 
+				for i in range(numberofGPU):
+					accumulate_list[i] += int(row[platform_list[i]])
+			else :
+				#str_temp = renderpassname+": "+str(accumulate)
+				#print(str_temp)
+				dict = {}
+				dict['Symbol']=renderpassname
+				#print (renderpassname)
+				for i in range(numberofGPU):
+					dict[platform_list[i]]=accumulate_list[i]
+					#print(str(accumulate_list[i]))
+				#print(dict)
+				dict_list.append(dict)
+				renderpassname = row.Symbol			
+				for i in range(numberofGPU):
+					accumulate_list[i] = int(row[platform_list[i]])
+	dict = {}
+	dict['Symbol']=renderpassname
+	for i in range(numberofGPU):
+		dict[platform_list[i]]=accumulate_list[i]
+	dict_list.append(dict)
+	renderpass_df= pandas.DataFrame(dict_list)
+	renderpass_df.to_csv("renderpass.csv")
+
 
 # save comparison_df to file
-    comparison_df.to_csv("comparison.csv")
+comparison_df.to_csv("comparison.csv")
 
 for l in get_peaks(comparison_df):
-    plt.annotate(l[0], l)
+	plt.annotate(l[0], l)
 
 # filter peak draw call and save to peak list file.
-    peak_df = comparison_df.iloc[peak_drawcall_no]
-    peak_df.to_csv("peakdrawcall.csv")
+	peak_df = comparison_df.iloc[peak_drawcall_no]
+	peak_df.to_csv("peakdrawcall.csv")
 
 
 # construct title
 title_str = ""
 for s in info_df.Platform:
-    title_str += s + ":"+str(int(1000*1000/comparison_df[s].values.sum()))+"FPS "
+	title_str += s + ":"+str(int(1000*1000/comparison_df[s].values.sum()))+"FPS "
 plt.title(title_str)
 plt.xlabel("No. of draw call")
 plt.ylabel("draw call cost(us)")
@@ -82,19 +121,19 @@ cursor = mplcursors.cursor(hover=True)
 
 @cursor.connect("add")
 def on_add(sel):
-    index = int(sel.target.index)
-    comment_str = ''
-    for (i, v) in comparison_df.iterrows():
-        if i == index:
-            for j in platform_list:
-                comment_str += " " + j + ":" + str(v[j]) + "us"
-            break
-    if symbol_list.__len__() > 0:
-        sel.annotation.set_text(
-            'Draw call No.{}\n{}\n{}'.format(int(sel.target.index), symbol_list[int(sel.target.index)], comment_str))
-    else:
-        sel.annotation.set_text(
-            'Draw call No.{}\n{}'.format(int(sel.target.index), comment_str))
+	index = int(sel.index)
+	comment_str = ''
+	for (i, v) in comparison_df.iterrows():
+		if i == index:
+			for j in platform_list:
+				comment_str += " " + j + ":" + str(v[j]) + "us"
+			break
+	if symbol_list.__len__() > 0:
+		sel.annotation.set_text(
+			'Draw call No.{}\n{}\n{}'.format(int(sel.index), symbol_list[int(sel.index)], comment_str))
+	else:
+		sel.annotation.set_text(
+			'Draw call No.{}\n{}'.format(int(sel.index), comment_str))
 
 
 plt.show()
